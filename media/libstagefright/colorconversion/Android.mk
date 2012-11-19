02ea229638e95c914357106f4e34bf9074f5e578
Ganster41 <ganster0x29@gmail.com>
2012-11-19 15:35:36 +0200

    Correct gralloc usage flags for renderer

--- media/libstagefright/colorconversion/Android.mk
+++ media/libstagefright/colorconversion/Android.mk
@@ -13,6 +13,10 @@ ifeq ($(TARGET_BOARD_PLATFORM), exynos4)
 LOCAL_CFLAGS += -DMALI_ALIGNMENT
 endif
 
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+        LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display/libgralloc 
+endif
+
 LOCAL_MODULE:= libstagefright_color_conversion
 
 include $(BUILD_STATIC_LIBRARY)
