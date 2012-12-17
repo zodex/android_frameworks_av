40a3e9a6fd00e9aee48c855aa43bfcee004039ca
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2012-12-18 01:38:49 +0400

    Fix compile error
    Change-Id: I24004bc81d85af9a419dc20b5277f983c8598edc

--- services/camera/libcameraservice/CameraClient.cpp
+++ services/camera/libcameraservice/CameraClient.cpp
@@ -83,9 +83,9 @@ status_t CameraClient::initialize(camera_module_t *module) {
             (void *)mCameraId);
 
     // Enable zoom, error, focus, and metadata messages by default
-    enableMsgType(CAMERA_MSG_ERROR | CAMERA_MSG_ZOOM | CAMERA_MSG_FOCUS |
+    enableMsgType(CAMERA_MSG_ERROR | CAMERA_MSG_ZOOM | CAMERA_MSG_FOCUS
 #ifndef QCOM_HARDWARE
-                  CAMERA_MSG_PREVIEW_METADATA
+                  | CAMERA_MSG_PREVIEW_METADATA
 #ifndef OMAP_ICS_CAMERA
                   | CAMERA_MSG_FOCUS_MOVE
 #endif
